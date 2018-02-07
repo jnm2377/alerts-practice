09f3fc92138376efe54de4521c9172c7129b2ce0
Josefina Mancilla <josefinanoemi4@gmail.com>
2018-02-07 13:51:37 -0600

    added player class with first question method and get total points method

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 console.log('connected to app.js');
+
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.points = 0;
+  }
+
+  firstQuestion() {
+    let userInput = prompt("George Washington was the first President on the United States. True or false?", "t or f?");
+    if (userInput.toUpperCase() === "T") {
+      this.points += 1;
+      alert("Correct! Next question.")
+    } else {
+      alert("Sorry, wrong answer. Better luck next time.")
+    }
+  }
+
+  secondQuestion() {}
+
+  getTotalPoints() {
+    if (this.points < 4) {
+      alert('You scored ' + this.points + '/5. You get a bronze star.');
+    } else if (this.points === 4) {
+      alert('You scored ' + this.points + '/5. You get a silver star.');
+    } else if (this.points === 5){
+      alert('You scored ' + this.points + '/5. You get a gold star.');
+    }
+  }
+}
+
+
+// ---------INSTANCES
+const player = new Player("Josefina");
+
+// ---------INITIATION
+
+player.firstQuestion();
+player.getTotalPoints();
